679ee68cda6a0baa296dc13c1986d98fcdf836da
Joan Lima <joanvc.lima@gmail.com>
2026-03-07 21:12:48 -0300

    fix: metro react dedup + npm install legacy-peer-deps on vercel

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Força instância única de React no bundle (corrige React error #527)
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "react":     path.resolve(__dirname, "node_modules/react"),
+  "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
